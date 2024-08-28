b968df10fd1712c21e66103c47043bd7c6c1e663
Jarl Briones <jarl@thinkingmachin.es>
2024-08-28 19:50:05 +0800

    first commit

---
+++ src/trial.py
@@ -0,0 +1,22 @@
+import argparse
+
+
+def main(args):
+    print(f"num_a: {args.num_a}")
+    print(f"num_b: {args.num_b}")
+    print(f"sum: {args.num_a + args.num_b}")
+
+
+def parse_arguments():
+    
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--num_a', required=True, type=float)
+    parser.add_argument('--num_b', required=True, type=float)
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    main(args)
